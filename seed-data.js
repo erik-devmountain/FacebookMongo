d0d05651df65e4f27e49b57071b4c10f925a56bb
Jacob Turner <j.israel.turner@gmail.com>
2015-02-11 23:10:57 -0700

    Ready.

---
+++ seed-data.js
@@ -0,0 +1,55 @@
+module.exports = [
+	{
+		"name": "Optimus Prime",
+		"composition": [
+			"Metal",
+			"Special Alien Materials",
+			"Alloys",
+			"Plastics"
+		],
+		"colors": [
+			"Red",
+			"Blue",
+			"White",
+			"Silver"
+		],
+		"faction": "Autobots"
+	},
+
+	{
+		"name": "Audi R8",
+		"manufacturer": "audi",
+		"cylinders": 8,
+		"topSpeed": 198,
+		"Gears": 7,
+		"doors": 2,
+		"msrp": 115000
+	},
+
+	{
+		"name": "James Bond",
+		"age": 40,
+		"nationality": "English",
+		"occupation": "British Spy",
+		"hasLicenseToKill": true, 
+		"weapons": [
+			{
+				"kind": "Hand Gun",
+				"name": "PP7",
+				"numberOfRounds": 8
+			},
+
+			{
+				"kind": "Rocket Launcher",
+				"name": "Rocket Launcher",
+				"numberOfRounds": 2
+			}
+		]
+	},
+
+	{
+		"_id": "audi",
+		"name": "Audi",
+		"nationality": "Germany"
+	}
+]
--- server.js
+++ server.js
@@ -1,15 +1,58 @@
+/*
+ *Faceboook-Mongo
+ *
+ *
+*/
+
+
+//modules
+
 var Express = require('express');
 var Session = require('express-session');
 var Passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
+var Mongoose = require('mongoose');
+var MongoJS = require('mongojs');
+
+//files
+
+var fakeData = require('./seed-data');
+
+var controller = require('./lib/controllers/main-control');
+
+//environment variables
 
 var App = Express();
+var port = 9001;
+var mongoURI = 'localhost:27017/facebook-mongo'
 
-//middleware
+//middleware  ==================================
 
 App.use(Session({secret: 'SOMETHING_SIKRIT'}));
 App.use(Passport.initialize());
 App.use(Passport.session());
+
+// Saves our agent
+
+// controller.saveAgent(fakeData[2]);
+
+// Saves our manufacturer
+
+// controller.saveManufacturer(fakeData[3]);
+
+// Saves our car
+
+// controller.saveCar(fakeData[1]);
+
+// Gets our car
+
+// controller.getCar();
+
+// Deletes an agent
+
+// controller.removeAgent('54dc438e2d5b94f9173a1f7b');
+
+
 
 Passport.use(new FacebookStrategy({
 	clientID: '767050560051516',
@@ -35,10 +78,10 @@ var isAuthed = function(req, res, next){
 	}
 }
 
+//endpoints  ==================================
 App.get('/me', isAuthed, function(req, res){
 	res.json(req.user);
 })
-
 
 App.get('/auth/facebook', Passport.authenticate('facebook'));
 
@@ -49,8 +92,39 @@ App.get('/auth/facebook/callback',
 			successRedirect: '/me',
 			failureRedirect: '/failure'
 		}
-	))
+	)
+)
 
-App.listen(9001, function(){
-	console.log('Now listening on port: 9001')
+
+//connection stuff ==========================
+
+var db = MongoJS('facebook-mongo', ['agents', 'cars', 'manufacturers']);
+
+// db.agents.save({
+// 	name: 'Jacob Turner',
+// 	age: true,
+// 	nationality: 27,
+// 	occupation: 'Undercover Mentor',
+// 	hasLicenseToKill: true,
+// 	weapons: [{
+// 		kind: 'OS',
+// 		name: {},
+// 		numberOfRounds: NaN,
+// 	}]
+// })
+
+// db.agents.find(function(err, docs){
+// 	console.log(docs);
+// })
+
+// db.agents.remove({name: 'Jacob Turner'});
+
+Mongoose.connect(mongoURI)
+
+Mongoose.connection.once('open', function(){
+	console.log('Successfully connected to Mongo via: ' + mongoURI)
+})
+
+App.listen(port, function(){
+	console.log('Now listening on port: ' + port)
 });
